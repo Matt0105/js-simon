a23f97f44e7c691fc8760e4e4ac72f9f026cde7b
Matt0105 <matteo0105@gmail.com>
2021-12-03 15:14:36 +0100

    filling user array

--- js/script.js
+++ js/script.js
@@ -14,9 +14,55 @@ function randomGenerator(array) {
     return numbers;
 }
 
+function printNumber(container, numbers) {
+
+    for(let i = 0; i < 5; i++) {
+        container.append(numbers[i] + " ");
+    }
+}
+
+function fillMyNumbers(guessArray) {
+
+    for(let i = 0; i < numbers.length; i++) {
+            
+        let myNum = parseInt(prompt(`Inserisci il ${i+1} numero da indovinare`));
+
+        while(myNum < 1 || isNaN(myNum)) {
+
+            myNum = parseInt(prompt(`Inserisci il ${i+1} numero da indovinare`));
+        }
+
+        guessArray.push(myNum);
+    }
+
+    return guessArray;
+}
+
 const container = document.querySelector(".container");
 
 let numbers = [];
 
 numbers = randomGenerator(numbers)
+
+printNumber(container, numbers);
+
+setTimeout(clearDom, 3000);
+
+function clearDom() {
+
+    container.innerHTML = "";
+
+    setTimeout(guess, 500);
+
+    function guess() {
+        let guessArray = [];
+
+        guessArray = fillMyNumbers(guessArray);
+
+        console.log(guessArray);
+
+    }
+
+    
+}
 
